aa3a43e4cf4768b79d6eca590846f402163794d6
RamonFossa <ramon.fossa@gmail.com>
2023-09-23 12:50:00 -0300

    feat: Creating test to sum function

--- test/sum_list_test.exs
+++ test/sum_list_test.exs
@@ -1,8 +1,19 @@
 defmodule SumListTest do
   use ExUnit.Case
-  doctest SumList
 
-  test "greets the world" do
-    assert SumList.hello() == :world
+  describe "sum/1" do
+    test "returns the list sum" do
+      list = [1, 2, 3]
+
+      response = SumList.sum(list)
+
+      expected_response = 6
+
+      assert response == expected_response
+
+      assert SumList.sum([3, 3, 6]) == 12
+
+      assert SumList.sum([-2, 2, -10, 8]) == -2
+    end
   end
 end
