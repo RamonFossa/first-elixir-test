af34b2107bfd793652cc4a9025712f28e0708229
RamonFossa <ramon.fossa@gmail.com>
2023-09-23 13:00:53 -0300

    feat: Using map from enum

--- lib/sum_list.ex
+++ lib/sum_list.ex
@@ -1,6 +1,8 @@
 defmodule SumList do
   def sum(list), do: sum(list, 0)
 
+  def call_map(list), do: Enum.map(list, fn element -> element + 5 end)
+
   defp sum([], acc), do: acc
 
   defp sum([head | tail], acc) do
